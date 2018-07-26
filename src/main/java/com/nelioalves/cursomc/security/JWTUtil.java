baa859b69d17b20b14aa5dc9b825c32a2f7421d9
DanielSantanaRibeiro <danielsantanaribeiro2015@gmail.com>
2018-07-25 23:52:22 -0300

    Implementando autorização e CORRIGINDO assinatura do token

--- src/main/java/com/nelioalves/cursomc/security/JWTUtil.java
+++ src/main/java/com/nelioalves/cursomc/security/JWTUtil.java
@@ -5,15 +5,16 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JWTUtil {
-	
+
 	@Value("${jwt.secret}")
 	private String secret;
-	
+
 	@Value("${jwt.expiration}")
 	private Long expiration;
 
@@ -21,7 +22,37 @@ public class JWTUtil {
 		return Jwts.builder()
 				.setSubject(username)
 				.setExpiration(new Date(System.currentTimeMillis() + expiration))
-				.signWith(SignatureAlgorithm.ES512, secret.getBytes())
+				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
 				.compact();
 	}
+
+	public boolean tokenValido(String token) {
+		Claims claims = getClaims(token);
+		if (claims != null) {
+			String username = claims.getSubject();
+			Date expirationDate = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+			if (username != null && expirationDate != null && now.before(expirationDate)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public String getUserName(String token) {
+		Claims claims = getClaims(token);
+		if(claims != null) {
+			return claims.getSubject();
+		}
+		return null;
+	}
+
+	private Claims getClaims(String token) {
+		try {
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
 }
